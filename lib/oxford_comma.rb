f50c7bc826d7b4c05e0900d0d9b7ae87669c89e0
Kyle Houghton <kylebenz@hotmail.com>
2018-12-23 23:39:10 +0000

    All tests pass.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[0..(array.length - 3)].join(", ") + ", " + array[(array.length - 2)..(array.length - 1)].join(", and ") 
+  end
 end
